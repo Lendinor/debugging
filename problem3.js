f6d835124404f3b8d8cd744a5e68413e092cf195
Alexander Flores <xandar@dosflores.net>
2017-01-10 14:39:51 -0800

    Finished

--- problem3.js
+++ problem3.js
@@ -1,16 +1,19 @@
 var theUsername = "appacademystudent"
-var thePassword = "p@ssword"
+var thePassword = "pizza"
 
 /* This function should only alert "Access granted" if theUsername is "appacademystudent" and thePassword is "pizza". If the username is incorrect, it should alert "I don't know that username.". If the username is correct but the password is wrong, it should alert "Access denied." */
 
-function checkPassword() {
-    if (theUsername == "appacademystudent") {
-        if (thePassword == "pizza") {
+function checkPassword() 
+{
+    if (theUsername == "appacademystudent")
+    {
+        if (thePassword == "pizza")
+        {
             alert("Access granted.")
-        } else {
+        } 
+        else 
+        {
             alert("Access denied.")
-        
-     else {
-        alert("I don't know that username.")
-     }
+        }
+    }
 }
